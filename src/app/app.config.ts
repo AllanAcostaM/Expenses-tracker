8e23225108ceff1491c1eb25e9015e4e35146548
Jossep Acosta <allanacost.27@gmail.com>
2025-03-18 18:54:46 -0500

    Performance firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 // app.config.ts
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { routes } from './app.routes';
@@ -7,6 +7,8 @@ import { routes } from './app.routes';
 // Importaciones de AngularFire
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { AngularFireModule } from '@angular/fire/compat';
+
 
 // Importa tu configuración de Firebase desde los entornos
 import { environment } from '../environments/environment';
@@ -18,6 +20,8 @@ export const appConfig: ApplicationConfig = {
     // Inicializa Firebase con la configuración de tu entorno
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     // Configura Firestore
-    provideFirestore(() => getFirestore())
+    provideFirestore(() => getFirestore()),
+    // Agrega la inicialización de AngularFire en versión compat para proveer el token requerido
+    importProvidersFrom(AngularFireModule.initializeApp(environment.firebase))
   ],
 };
